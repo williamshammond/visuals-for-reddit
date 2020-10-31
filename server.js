b2dedf4c539271dd07457c3059487b263c0a5330
williamshammond <williamhammond44@gmail.com>
2020-10-31 16:43:01 -0700

    Added the main .json and .js file for the website.

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT ||
+    5000;
+
+app.listen(PORT, () => {
+    console.log("listening on " + PORT);
+});
+
+app.get('/', (req, res) => {
+    res.send({ok:true});
+})
+
